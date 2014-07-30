4ca08f1abfdbb129d5d1e90d3bb078280eccc1dc
Richard Selneck <blakpilar@gmail.com>
2014-07-29 21:08:27 -0400

    Reorganized export.h

--- include/noise/export.h
+++ include/noise/export.h
@@ -6,13 +6,11 @@
 #    define NOISE_EXPORT __attribute__((__visibility__("default")))
 #  elif defined( _MSC_VER )
 #    define NOISE_EXPORT __declspec(dllexport)
+#  else
+#    error Could not detect compiler for exporting
 #  endif
 #else
 #  define NOISE_EXPORT
-#endif
-
-#if !defined( NOISE_EXPORT )
-#  error Could not create EXPORT macro
 #endif
 
 #endif
